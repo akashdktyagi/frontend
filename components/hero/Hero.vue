9c78581fe6a8a0d5c45f8ed034c64b58a6ffc9f9
akashtyagi <akashdktyagi@gmail.com>
2021-12-22 23:27:36 +0800

    first commit

---
+++ components/hero/Hero.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Welcome to Yantra Cloud E Commerce App!
+        </h1>
+        <h2 class="subtitle">
+          This is a project to implement State of the Art Software Architecture principals and techniques.
+          Feel Free to be part of the project. Reach me out at: akashdktyagi@gmail.com
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      background: rgb(81, 186, 252);
+    }
+    h1, h2 {
+      color: #fff;
+    }
+</style>
